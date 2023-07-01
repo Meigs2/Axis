ebc7ab109c65de9f6a4e63f0b59681756f8e55a1
meigs2 <connorradeloff1@gmail.com>
2023-07-01 13:17:41 -0500

    Initial Commit

---
+++ src/Axis.Client/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ['./**/*.{razor,html}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                background: {
+                    DEFAULT: '#262A56',
+                    dark: '#0D0E29',
+                },
+                primary: {
+                    DEFAULT: '#B8621B'
+                },
+                focus:{
+                    DEFAULT: '#E3CCAE'
+                } 
+            }
+        },
+    },
+    plugins: [],
+}
